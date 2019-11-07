3bcb29892a4c23df5dcf7b5649aa29b6235cf4d5
zhongqian <253503125@qq.com>
2019-11-07 23:58:42 +0800

    添加资源表dao,service,controller

--- src/main/java/com/sys/web/convert/UserRoleConvert.java
+++ src/main/java/com/sys/web/convert/UserRoleConvert.java
@@ -1,7 +1,8 @@
 package com.sys.web.convert;
 
-import com.sys.web.controller.vo.UserRoleVO;
+
 import com.sys.entity.UserRoleDO;
+import com.sys.web.vo.UserRoleVO;
 
 import java.util.ArrayList;
 import java.util.List;
